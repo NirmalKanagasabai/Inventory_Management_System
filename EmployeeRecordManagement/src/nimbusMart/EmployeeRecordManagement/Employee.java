f92669ced11c0a9939ddadc3a6196deab74d51bf
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-03-10 23:47:41 -0400

    Switching Project Name

--- EmployeeRecordManagement/src/nimbusMart/EmployeeRecordManagement/Employee.java
+++
@@ -1,9 +0,0 @@
-package nimbusMart.EmployeeRecordManagement;
-
-public class Employee {
-	
-	public static void main(String[] args) throws Exception {
-		
-	}
-
-}
---
+++ InventoryManagement/src/nimbusMart/InventoryManagement/Manager.java
@@ -0,0 +1,24 @@
+package nimbusMart.InventoryManagement;
+
+import java.sql.*;
+
+
+public class Manager {
+	
+	public static void main(String[] args) throws Exception {
+		
+		String url = "";
+		String userName = "root";
+		String password = "Kanaasw23";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		Connection connection = DriverManager.getConnection(url, userName, password);
+		//Statement statement = 
+		
+		
+		
+	}
+
+}
+  
